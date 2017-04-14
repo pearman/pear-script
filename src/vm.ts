a319c112dcf641184dde1bd7cce688bcb42cce36
Gabe Pearhill <gabepearhill@gmail.com>
2017-04-13 20:41:16 -0600

    Jeepers! Stuff is working!

--- src/vm.ts
+++ src/vm.ts
@@ -10,38 +10,45 @@ export class Vm {
 
     memory = {};
 
-    execute(parseTree, acc = this.memory) {
-        let output = _.reduce(parseTree, (acc: any, statement: any) => {
-            if (_.isArray(statement)) {
-                return _.reduce(statement, (acc, value) => {
-                    return this.execute([value], acc);
-                }, acc);
-            } else {
-                statement = this.wrapPrimitive(statement);
-                switch(statement.type) {
-                    case 'assignment': 
-                        var address = _.isArray(statement.parent) ?
-                            _.map(statement.parent, 'value'):
-                            statement.parent.value;
-                        return _.set(acc, address, this.execute(_.isArray(statement.child) ? statement.child : [statement.child]));
-                    case 'property':
-                        return _.get(acc, statement.value);
-                    case 'method':
-                        let method: any = _.get(acc, statement.method);
-                        let args = _.map(statement.args, arg => this.execute([arg]));
-                        if (_.isFunction(method)) {
-                            args.unshift(acc);
-                            return this.wrapPrimitive(method(args));
-                        } else {
-                            let mappedArgs = _.map(method.args, (arg: any, i) => ({[arg.value] : args[i]}))
-                            let scope = _.assign({}, this.memory, ...mappedArgs);
-                            return this.wrapPrimitive(this.execute(method.block, scope));
-                        }
-                    default: return statement;
-                }
+    execute(parseTree, acc = this.memory, level = 0) {
+        while (parseTree.length > 0) {
+            let statement = parseTree.shift();
+            //console.log(`${level} SHIFT`, statement);
+            this.reduce(statement, acc, this.memory, level);
+        }
+        //return output;
+    }
+
+    reduce(statement, acc, rootScope, level) {
+        //console.log(`${level} REDUCE`, statement, acc);
+        if (_.isArray(statement)) {
+            return _.reduce(statement, (acc, value, i) => {
+                return this.reduce(value, acc, rootScope, ++level);
+            }, acc);
+        } else {
+            statement = this.wrapPrimitive(statement);
+            switch(statement.type) {
+                case 'assignment': 
+                    var address = _.isArray(statement.parent) ?
+                        _.map(statement.parent, 'value'):
+                        statement.parent.value;
+                    return _.set(acc, address, this.reduce(statement.child, acc, rootScope, ++level));
+                case 'property':
+                    return _.get(_.merge({}, rootScope, acc), statement.value);
+                case 'method':
+                    let method: any = _.get(acc, statement.method);
+                    let args = _.map(statement.args, arg => this.reduce(arg, acc, rootScope, ++level));
+                    if (_.isFunction(method)) {
+                        args.unshift(acc);
+                        return this.wrapPrimitive(method(args));
+                    } else {
+                        let mappedArgs = _.map(method.args, (arg: any, i) => ({[arg.value] : args[i]}))
+                        let tableScope = _.assign({}, this.memory, ...mappedArgs);
+                        return this.wrapPrimitive(this.reduce(method.block, tableScope, tableScope, ++level));
+                    }
+                default: return statement;
             }
-        }, acc);
-        return output;
+        }
     }
 
     wrapPrimitive(statement) {
